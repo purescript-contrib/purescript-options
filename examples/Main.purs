b0a70e6b7a579527939b99ef24eb5ea6bef2425c
eric thul <thul.eric@gmail.com>
2014-11-23 12:43:47 -0500

    Options implementation

---
+++ examples/Main.purs
@@ -0,0 +1,43 @@
+module Main where
+
+import Data.Foreign (Foreign())
+import Data.Maybe (Maybe(..))
+import Data.Options (Option(), IsOption, optionFn, options, (:=))
+import Debug.Trace
+
+data Shape = Circle | Square | Triangle
+
+instance shapeShow :: Show Shape where
+  show Circle = "circle"
+  show Square = "square"
+  show Triangle = "triangle"
+
+instance shapeIsOption :: IsOption Shape where
+  (:=) k a = (optionFn k) := show a
+
+foreign import data Foo :: *
+
+foreign import foo "var foo = 'foo';" :: Option Foo String
+
+foreign import bar "var bar = 'bar';" :: Option Foo Number
+
+foreign import baz "var baz = 'baz';" :: Option Foo (Maybe String)
+
+foreign import fiz "var fiz = 'fiz';" :: Option Foo (Maybe String)
+
+foreign import biz "var biz = 'biz';" :: Option Foo Shape
+
+opts = foo := "aaa" <>
+       bar := 10 <>
+       baz := Just "c" <>
+       fiz := Nothing <>
+       biz := Square
+
+main = (trace <<< showForeign <<< options) opts
+
+foreign import showForeign
+  """
+    function showForeign(a){
+      return JSON.stringify(a);
+    }
+  """ :: Foreign -> String
---
+++ src/Data/Options.purs
@@ -0,0 +1,71 @@
+module Data.Options
+  ( Options()
+  , Option()
+  , IsOption
+  , optionFn
+  , options
+  , (:=)
+  ) where
+
+import Data.Foreign (Foreign())
+import Data.Function (Fn2(), runFn2)
+import Data.Maybe (Maybe(..))
+import Data.Monoid (Monoid)
+
+foreign import data Options :: * -> *
+
+foreign import data Option :: * -> * -> *
+
+infixr 6 :=
+
+class IsOption r where
+  (:=) :: forall a. Option a r -> r -> Options a
+
+foreign import optionFn "function optionFn(a){return a;}" :: forall r s a. Option a r -> Option a s
+
+instance optionsSemigroup :: Semigroup (Options a) where
+  (<>) = runFn2 appendFn
+
+instance optionsMonoid :: Monoid (Options a) where
+  mempty = memptyFn
+
+instance stringIsOption :: IsOption String where
+  (:=) = runFn2 primIsOptionFn
+
+instance numberIsOption :: IsOption Number where
+  (:=) = runFn2 primIsOptionFn
+
+instance maybeIsOption :: (IsOption a) => IsOption (Maybe a) where
+  (:=) k Nothing = memptyFn
+  (:=) k (Just a) = (optionFn k) := a
+
+foreign import appendFn
+  """
+    function appendFn(o1, o2){
+      return o1.concat(o2);
+    }
+  """ :: forall a. Fn2 (Options a) (Options a) (Options a)
+
+foreign import memptyFn "var memptyFn = [];" :: forall a. Options a
+
+foreign import primIsOptionFn
+  """
+    function primIsOptionFn(k, v) {
+      return [[k, v]];
+    }
+  """ :: forall b a. Fn2 (Option b a) a (Options b)
+
+foreign import options
+  """
+    function options(o){
+      var res = {};
+      var i = -1;
+      var n = o.length;
+      while(++i < n) {
+        var k = o[i][0];
+        var v = o[i][1];
+        res[k] = v;
+      }
+      return res;
+    }
+  """ :: forall a. Options a -> Foreign
