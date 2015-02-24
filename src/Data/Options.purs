b6c5f8a0bfe81b76da9c49f071d1b9368ec3056e
eric thul <thul.eric@gmail.com>
2015-02-23 22:15:25 -0500

    Adding additional instances
    Adding instances for records, functions, and arrays.

--- src/Data/Options.purs
+++ src/Data/Options.purs
@@ -23,52 +23,72 @@ class IsOption r where
 
 foreign import optionFn "function optionFn(a){return a;}" :: forall r s a. Option a r -> Option a s
 
-instance optionsSemigroup :: Semigroup (Options a) where
+instance semigroupOptions :: Semigroup (Options a) where
   (<>) = runFn2 appendFn
 
-instance optionsMonoid :: Monoid (Options a) where
+instance monoidOptions :: Monoid (Options a) where
   mempty = memptyFn
 
-instance stringIsOption :: IsOption String where
-  (:=) = runFn2 primIsOptionFn
+instance isOptionString :: IsOption String where
+  (:=) = runFn2 isOptionPrimFn
 
-instance booleanIsOption :: IsOption Boolean where
-  (:=) = runFn2 primIsOptionFn
+instance isOptionBoolean :: IsOption Boolean where
+  (:=) = runFn2 isOptionPrimFn
 
-instance numberIsOption :: IsOption Number where
-  (:=) = runFn2 primIsOptionFn
+instance isOptionNumber :: IsOption Number where
+  (:=) = runFn2 isOptionPrimFn
 
-instance maybeIsOption :: (IsOption a) => IsOption (Maybe a) where
+instance isOptionRecord :: IsOption { | a } where
+  (:=) = runFn2 isOptionPrimFn
+
+instance isOptionFunction :: IsOption (a -> b) where
+  (:=) = runFn2 isOptionPrimFn
+
+instance isOptionArray :: (IsOption a) => IsOption [a] where
+  (:=) k vs = joinFn $ (:=) (optionFn k) <$> vs
+
+instance isOptionMaybe :: (IsOption a) => IsOption (Maybe a) where
   (:=) k Nothing = memptyFn
   (:=) k (Just a) = (optionFn k) := a
 
-foreign import appendFn
-  """
-    function appendFn(o1, o2){
-      return o1.concat(o2);
-    }
-  """ :: forall a. Fn2 (Options a) (Options a) (Options a)
-
 foreign import memptyFn "var memptyFn = [];" :: forall a. Options a
 
-foreign import primIsOptionFn
-  """
-    function primIsOptionFn(k, v) {
-      return [[k, v]];
-    }
-  """ :: forall b a. Fn2 (Option b a) a (Options b)
+foreign import appendFn """
+  function appendFn(o1, o2) {
+    return o1.concat(o2);
+  }
+""" :: forall a. Fn2 (Options a) (Options a) (Options a)
 
-foreign import options
-  """
-    function options(o){
-      var res = {};
-      var i = -1;
-      var n = o.length;
-      while(++i < n) {
-        var k = o[i][0];
-        var v = o[i][1];
-        res[k] = v;
-      }
-      return res;
+foreign import joinFn """
+  function joinFn(os) {
+    var k = null;
+    var vs = [];
+    var i = -1;
+    var n = os.length;
+    while(++i < n) {
+      k = os[i][0][0];
+      vs.push(os[i][0][1]);
     }
-  """ :: forall a. Options a -> Foreign
+    return [[k, vs]];
+  }
+""" :: forall a b. [Options a] -> Options b
+
+foreign import isOptionPrimFn """
+  function isOptionPrimFn(k, v) {
+    return [[k, v]];
+  }
+""" :: forall b a. Fn2 (Option b a) a (Options b)
+
+foreign import options """
+  function options(o) {
+    var res = {};
+    var i = -1;
+    var n = o.length;
+    while(++i < n) {
+      var k = o[i][0];
+      var v = o[i][1];
+      res[k] = v;
+    }
+    return res;
+  }
+""" :: forall a. Options a -> Foreign
