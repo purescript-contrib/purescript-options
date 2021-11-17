5a79052e549a6baf4c19d1e97d918d35d6071c6e
Thomas Honeyman <admin@thomashoneyman.com>
2021-11-17 10:53:07 -0500

    Introduce purs-tidy formatter (#45)
    * Add purs-tidy formatter
    
    * Run purs-tidy
    
    * Update test/Test/Main.purs

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -44,14 +44,14 @@ anArrayOfShapesOption :: Option MyOptions (Array Shape)
 anArrayOfShapesOption = cmap (map show) (opt "anArrayOfShapesOption")
 
 myOptions :: Options MyOptions
-myOptions = aStringOption := "aaa" <>
-       anIntOption := 10 <>
-       aBooleanOption := true <>
-       anOptionalStringOption := Just "c" <>
-       anotherOptionalStringOption := Nothing <>
-       aShapeOption := Square <>
-       aFunctionOption := (\a b c -> a + b + c) <>
-       anArrayOfShapesOption := [Square, Circle, Triangle]
+myOptions = aStringOption := "aaa"
+  <> anIntOption := 10
+  <> aBooleanOption := true
+  <> anOptionalStringOption := Just "c"
+  <> anotherOptionalStringOption := Nothing
+  <> aShapeOption := Square
+  <> aFunctionOption := (\a b c -> a + b + c)
+  <> anArrayOfShapesOption := [ Square, Circle, Triangle ]
 
 foreign import showForeign :: Foreign -> String
 
@@ -76,46 +76,46 @@ shouldEqual actual expected = assertEqual { actual, expected }
 
 main :: Effect Unit
 main = do
-        describe "end-to-end" do
-          it "works as expected" do
-            let expected = """{"aStringOption":"aaa","anIntOption":10,"aBooleanOption":true,"anOptionalStringOption":"c","shape":"square","anArrayOfShapesOption":["square","circle","triangle"]}"""
-            let actual = showForeign $ options myOptions
+  describe "end-to-end" do
+    it "works as expected" do
+      let expected = """{"aStringOption":"aaa","anIntOption":10,"aBooleanOption":true,"anOptionalStringOption":"c","shape":"square","anArrayOfShapesOption":["square","circle","triangle"]}"""
+      let actual = showForeign $ options myOptions
 
-            actual `shouldEqual` expected
-        describe "opt" do
-          it "works with `String`s" do
-            let expected = """{"aStringOption":"hello, world"}"""
-            let actual = showForeign $ options $ (:=) aStringOption "hello, world"
+      actual `shouldEqual` expected
+  describe "opt" do
+    it "works with `String`s" do
+      let expected = """{"aStringOption":"hello, world"}"""
+      let actual = showForeign $ options $ (:=) aStringOption "hello, world"
 
-            actual `shouldEqual` expected
-          it "works with `Int`s" do
-            let expected = """{"anIntOption":74}"""
-            let actual = showForeign $ options $ (:=) anIntOption 74
+      actual `shouldEqual` expected
+    it "works with `Int`s" do
+      let expected = """{"anIntOption":74}"""
+      let actual = showForeign $ options $ (:=) anIntOption 74
 
-            actual `shouldEqual` expected
-          it "works with `Boolean`s" do
-            let expected = """{"aBooleanOption":true}"""
-            let actual = showForeign $ options $ (:=) aBooleanOption true
+      actual `shouldEqual` expected
+    it "works with `Boolean`s" do
+      let expected = """{"aBooleanOption":true}"""
+      let actual = showForeign $ options $ (:=) aBooleanOption true
 
-            actual `shouldEqual` expected
-          it "works with `Array`s" do
-            let expected = """{"anArrayOfShapesOption":["square","circle","triangle"]}"""
-            let actual = showForeign $ options $ (:=) anArrayOfShapesOption [Square, Circle, Triangle]
+      actual `shouldEqual` expected
+    it "works with `Array`s" do
+      let expected = """{"anArrayOfShapesOption":["square","circle","triangle"]}"""
+      let actual = showForeign $ options $ (:=) anArrayOfShapesOption [ Square, Circle, Triangle ]
 
-            actual `shouldEqual` expected
-          it "does nothing for functions" do
-            let expected = """{}"""
-            let actual = showForeign $ options $ (:=) aFunctionOption (\a b c -> a + b + c)
+      actual `shouldEqual` expected
+    it "does nothing for functions" do
+      let expected = """{}"""
+      let actual = showForeign $ options $ (:=) aFunctionOption (\a b c -> a + b + c)
 
-            actual `shouldEqual` expected
-        describe "optional" do
-          it "includes the option when it is provided" do
-            let expected = """{"anotherOptionalStringOption":"provided"}"""
-            let actual = showForeign $ options $ (:=) anotherOptionalStringOption $ Just "provided"
+      actual `shouldEqual` expected
+  describe "optional" do
+    it "includes the option when it is provided" do
+      let expected = """{"anotherOptionalStringOption":"provided"}"""
+      let actual = showForeign $ options $ (:=) anotherOptionalStringOption $ Just "provided"
 
-            actual `shouldEqual` expected
-          it "excludes the option when it is not provided" do
-            let expected = """{}"""
-            let actual = showForeign $ options $ (:=) anotherOptionalStringOption Nothing
+      actual `shouldEqual` expected
+    it "excludes the option when it is not provided" do
+      let expected = """{}"""
+      let actual = showForeign $ options $ (:=) anotherOptionalStringOption Nothing
 
-            actual `shouldEqual` expected
+      actual `shouldEqual` expected
